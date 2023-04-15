391b4259a70145c39ef5aaebc475f46ea567418a
Motteo1 <timothykirungu@gmail.com>
2023-04-15 03:42:44 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,3 +8,21 @@ from typing import Callable, Optional, Union
 from uuid import uuid4
 
 def replay(method: Callable):
+    """Replay function"""
+    key = method.__qualname__
+    p = "".join([key, ":inputs"])
+    q = "".join([key, ":outputs"])
+    count = method.__self__get(key)
+    p_list = method.__self__.lrange(p, 0, -1)
+    q_list = method.__self__.lrange(q, 0, -1)
+    queue = list(zip(p_list, q_list))
+    print(f"{key} was called {decode_utf8(count)} times:")
+    for k, v, in queue:
+        k = decode_utf8(k)
+        v = decode_utf8(v)
+        print(f"{key}(*{k}) -> {v}")
+
+
+def call_history(method: Callable) -> Callable:
+    """ Call History """
+    key = method.__qualname__
